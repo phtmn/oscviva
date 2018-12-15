d5996deae66ce0ab98b95f443ed7aedcd2c906ad
Matthaus Nawan de Souza e Silva <matthausnawan@gmail.com>
2018-12-15 00:27:34 -0300

    pastas guia

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,12 @@
 Route::get('/', function () {
     return redirect()->route('login');
 });
+
 Auth::routes();
 Route::get('/dashboard','DashboardController@index')->name('dashboard')->middleware('auth');
 
 //Todas rotas desse grupo estão protegidas por autenticação!!!
 Route::group(['middleware'=>'auth','prefix'=>'admin'], function(){
-    
-
 
     Route::group(['namespace'=>'Administrativo'],function(){
         // Criar aqui dentro todos os controles referente ao modulo administrativo
